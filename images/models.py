05eb12f89cb706790f8f515b8723db2af15d10a3
erhic <eric.gichovi@student.moringaschool.com>
2022-02-26 10:35:09 +0300

    set the foreign key field by default to allow null or empty fields

--- images/models.py
+++ images/models.py
@@ -1,6 +1,5 @@
-from email.mime import image
+
 from time import timezone
-from turtle import mode
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -11,8 +10,8 @@ class Image(models.Model):
     image=models.FileField()
     image_name=models.CharField(max_length=150)
     image_descr=models.TextField(max_length=1000)
-    image_location=models.ForeignKey('Category',on_delete=models.SET_NULL)
-    image_category=models.ForeignKey('Location',on_delete=models.SET_NULL)
+    image_location=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+    image_category=models.ForeignKey('Location',on_delete=models.SET_NULL,null=True)
     user=models.ForeignKey(User ,on_delete=models.CASCADE)
     date_posted=models.DateTimeField(default=timezone.now)
     time_posted=models.DateTimeField(auto_now_add=True)
