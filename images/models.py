ccf11e3a9f70345cae2020401150be803a9f0ea0
erhic <eric.gichovi@student.moringaschool.com>
2022-02-26 21:27:21 +0300

    updated models,table cols

--- images/models.py
+++ images/models.py
@@ -1,4 +1,6 @@
-
+import cloudinary
+from cloudinary.models import CloudinaryField
+from distutils.command.upload import upload
 from time import timezone
 from django.db import models
 from django.utils import timezone
@@ -8,13 +10,65 @@ from django.contrib.auth.models import User
 
 class Image(models.Model):
     name=models.CharField(max_length=150)
-    image=models.FileField()
-    descr=models.TextField(max_length=1000)
+    image = CloudinaryField('image')
+    description=models.TextField(max_length=1000)
     location=models.ForeignKey('Location',on_delete=models.SET_NULL,null=True)
     category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
     user=models.ForeignKey(User ,on_delete=models.CASCADE)
     date_posted=models.DateTimeField(default=timezone.now)
     time_posted=models.DateTimeField(auto_now_add=True)
+    
+    # save image to database
+    def save_image(self):
+        self.save()
+
+
+    # update image
+    def update_image(self, name, description, location, category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+    # get image by id
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
+        return image
+
+    # get images by location
+    @classmethod
+    def filter_by_location(cls, location):
+        images = Image.objects.filter(location__name=location)
+        return images
+
+    # get images by category
+    @classmethod
+    def filter_by_category(cls, category):
+        images = Image.objects.filter(category__name=category)
+        return images
+
+    # search images
+    @classmethod
+    def search_image(cls, search_term):
+        images = cls.objects.filter(name__icontains=search_term)
+        return images
+
+    # delete image from database
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+    
+    
     
     def __str__(self):
         return self.name
@@ -22,14 +76,46 @@ class Image(models.Model):
 class Category(models.Model):
     category=models.TextField(max_length=150)
     
+    # save category to database
+    def save_category(self):
+        self.save()
+
+    # update category
+    def update_category(self, name):
+        self.name = name
+        self.save()
+
+    # delete category from database
+    def delete_category(self):
+        self.delete()
+
     def __str__(self):
-        return self.category
+        return self.name
+
+    
+    
     
 
     
     
 class Location(models.Model):
-    location=models.TextField(max_length=250)
+    name =models.TextField(max_length=250)
     
+    # save location to database
+    def save_location(self):
+        self.save()
+
+    # update location
+    def update_location(self, name):
+        self.name = name
+        self.save()
+
+     # delete location from database
+    def delete_location(self):
+        self.delete()
+
     def __str__(self):
-        return self.location
+        return self.name
+
+
+
